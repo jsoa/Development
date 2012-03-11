ac2d4a8856ecf4f2426d49e6fc86dc8fd640debf
Jose Soares <jose@linux.com>
2012-03-10 20:59:54 -0500

    Added some bindings

---
+++ dotfiles/emacs.d/jsoa/bindings.el
@@ -0,0 +1,10 @@
+;; map the window manipulation keys to meta 0, 1, 2, o
+(global-set-key (kbd "M-3") 'split-window-horizontally) ; was digit-argument
+(global-set-key (kbd "M-2") 'split-window-vertically) ; was digit-argument
+(global-set-key (kbd "M-1") 'delete-other-windows) ; was digit-argument
+(global-set-key (kbd "M-0") 'delete-window) ; was digit-argument
+(global-set-key (kbd "M-o") 'other-window) ; was facemenu-keymap
+
+(global-set-key (kbd "M-O") 'rotate-windows)
+
+;; Note: need to disable menu access keys and shortcuts in terminal
