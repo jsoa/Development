54284bf7fa71d53275edba01947bbe4c3c4b2f89
Jose Soares <jose@linux.com>
2012-03-12 21:38:56 -0400

    Added some coffee-mode configurations for emacs

---
+++ dotfiles/emacs.d/jsoa/coffee-mode.el
@@ -0,0 +1,8 @@
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+(add-hook 'coffee-mode-hook '(lambda () (coffee-cos-mode t)))
