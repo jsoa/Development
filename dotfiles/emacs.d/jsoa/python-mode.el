3877307c5d943dc01352a2c443329b57ec8ee7b9
Jose Soares <jose@linux.com>
2012-03-22 18:26:56 -0400

    Removed electric pairs in python-mode

--- dotfiles/emacs.d/jsoa/python-mode.el
+++ dotfiles/emacs.d/jsoa/python-mode.el
@@ -10,18 +10,18 @@
 (autoload 'pymacs-load "pymacs" nil t)
 
 ;;; Electric Pairs
-(add-hook 'python-mode-hook
-     (lambda ()
-      (define-key python-mode-map "\"" 'electric-pair)
-      (define-key python-mode-map "\'" 'electric-pair)
-      (define-key python-mode-map "(" 'electric-pair)
-      (define-key python-mode-map "[" 'electric-pair)
-      (define-key python-mode-map "{" 'electric-pair)))
-(defun electric-pair ()
-  "Insert character pair without sournding spaces"
-  (interactive)
-  (let (parens-require-spaces)
-    (insert-pair)))
+;;(add-hook 'python-mode-hook
+;;     (lambda ()
+;;      (define-key python-mode-map "\"" 'electric-pair)
+;;      (define-key python-mode-map "\'" 'electric-pair)
+;;      (define-key python-mode-map "(" 'electric-pair)
+;;      (define-key python-mode-map "[" 'electric-pair)
+;;      (define-key python-mode-map "{" 'electric-pair)))
+;;(defun electric-pair ()
+;;  "Insert character pair without sournding spaces"
+;;  (interactive)
+;;  (let (parens-require-spaces)
+;;    (insert-pair)))
 
 ;;; bind RET to py-newline-and-indent
 (add-hook 'python-mode-hook '(lambda () 
