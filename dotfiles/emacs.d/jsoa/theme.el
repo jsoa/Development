791173ccca6069743aafc66c4a3a5bb980da1910
Jose Soares <jose@linux.com>
2012-03-25 00:30:59 -0400

    Added color theme and my custom theme

--- dotfiles/emacs.d/jsoa/theme.el
+++ dotfiles/emacs.d/jsoa/theme.el
@@ -1,1 +1,32 @@
-;; TODO: add theme
+(require 'color-theme)
+
+(setq color-theme-is-global t)
+
+(defun color-theme-lowlevel ()
+  "LowLevel Theme"
+  (interactive)
+  (color-theme-install
+   '(color-theme-lowlevel
+     ((background-color . "#000000")
+      (foreground-color . "#B9BDB6")
+      (background-mode . dark)
+      (border-color . "#323232")
+      (cursor-color . "#FFFFFF")
+      (mouse-color . "#323232"))
+     (mode-line ((t (:foreground "#00000070" :background "#323232"))))
+     (region ((t (:background "#323232"))))
+
+     (font-lock-comment-face ((t (:foreground "#646763"))))
+     (font-lock-constant-face ((t (:foreground "#6E9360"))))
+     (font-lock-builtin-face ((t (:foreground "#5CE638"))))
+     (font-lock-function-name-face ((t (:foreground "#FFAA3E"))))
+     (font-lock-variable-name-face ((t (:foreground "#FFFFFF"))))
+     (font-lock-keyword-face ((t (:foreground "#5BA1CF"))))
+     (font-lock-string-face ((t (:foreground "#6E9360"))))
+     (font-lock-integer-face ((t (:foreground "#5CE638"))))
+     (font-lock-doc-string-face ((t (:foreground "#68DC0C"))))
+     (font-lock-type-face ((t (:foreground "#FFFFFF"))))
+     )))
+
+(provide 'color-theme-lowlevel)
+(color-theme-lowlevel)
