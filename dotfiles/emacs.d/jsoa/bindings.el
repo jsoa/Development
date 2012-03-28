850b0a4b3ae4eb3cff758ac2dd11d9ef40efe032
Jose Soares <josefsoares@gmail.com>
2012-03-28 14:59:40 -0400

    Removed the rotate windows function and key binding; Removed some unused bindings

--- dotfiles/emacs.d/jsoa/bindings.el
+++ dotfiles/emacs.d/jsoa/bindings.el
@@ -1,11 +1,10 @@
+
 ;; map the window manipulation keys to meta 0, 1, 2, o
 (global-set-key (kbd "M-3") 'split-window-horizontally) ; was digit-argument
 (global-set-key (kbd "M-2") 'split-window-vertically) ; was digit-argument
 (global-set-key (kbd "M-1") 'delete-other-windows) ; was digit-argument
 (global-set-key (kbd "M-0") 'delete-window) ; was digit-argument
 (global-set-key (kbd "M-o") 'other-window) ; was facemenu-keymap
-
-(global-set-key (kbd "M-O") 'rotate-windows)
 
 ;; ibuffer > list-buffers
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -15,7 +14,5 @@
 (global-set-key (kbd "M-K") 'kill-this-buffer)
 
 (global-set-key (kbd "M-g") 'goto-line)
-;;(global-set-key "\C-w" 'bookmark-jump)
-;;(global-set-key (kbd "C-e") 'bookmark-set)
 
 ;; Note: need to disable menu access keys and shortcuts in terminal
