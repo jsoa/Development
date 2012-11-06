f975a336f34a48799ecbe6a086a3ef3b9aedd3ff
Jose Soares <josefsoares@gmail.com>
2012-11-06 16:23:03 -0500

    Updated some vendors

--- dotfiles/emacs.d/jsoa/coffee-mode.el
+++ dotfiles/emacs.d/jsoa/coffee-mode.el
@@ -1,8 +1,9 @@
 (defun coffee-custom ()
   "coffee-mode-hook"
- (set (make-local-variable 'tab-width) 2))
+ (set (make-local-variable 'tab-width) 2)
+ (setq coffee-args-compile '("-c" "-o" "../")))
 
 (add-hook 'coffee-mode-hook
   '(lambda() (coffee-custom)))
 
-;; (add-hook 'coffee-mode-hook '(lambda () (coffee-cos-mode t)))
+(add-hook 'coffee-mode-hook '(lambda () (coffee-cos-mode t)))
---
+++ dotfiles/emacs.d/jsoa/rst-mode.el
@@ -0,0 +1,11 @@
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(rst-level-1-face ((t (:background "#323232"))) t)
+ '(rst-level-2-face ((t (:background "#323232"))) t)
+ '(rst-level-3-face ((t (:background "#323232"))) t)
+ '(rst-level-4-face ((t (:background "#323232"))) t)
+ '(rst-level-5-face ((t (:background "#323232"))) t)
+)
