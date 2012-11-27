9c8408a95e7f9be02ec92760eef2314fca5f3161
Jose Soares <josefsoares@gmail.com>
2012-11-27 09:47:47 -0500

    Initial import of python/django snippets

--- dotfiles/emacs.d/jsoa/yasnippet.el
+++ dotfiles/emacs.d/jsoa/yasnippet.el
@@ -1,5 +1,10 @@
 
 (yas/global-mode 1)
 
+(setq yas/triggers-in-field t)
+(setq yas/prompt-functions '(yas/ido-prompt))
+
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
 (yas/load-directory "~/.emacs.d/jsoa/snippets")
+
+
