a9b23f044a1ad49a121e7a7463edb32339bc2136
Jose Soares <josefsoares@gmail.com>
2012-12-11 15:18:36 -0500

    Added magit specific diff face (when section is highlighted)

--- dotfiles/emacs.d/jsoa/diff.el
+++ dotfiles/emacs.d/jsoa/diff.el
@@ -1,3 +1,6 @@
 
 (defface diff-added '((t (:inherit diff-changed :foreground "#70FB67"))) nil)
 (defface diff-removed '((t (:inherit diff-changed :foreground "#FF453C"))) nil)
+
+;; Magit diffs
+(defface magit-item-highlight '((t (:inherit magit-item-highlight :background "#222"))) nil)
