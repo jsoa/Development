d253aa02cb8def777d1e4cbf62c5fdc5e241d261
Jose Soares <jose@linux.com>
2012-12-17 18:47:09 -0500

    [-] Added python-mode binds for pylint and pep8

--- dotfiles/emacs.d/jsoa/python-mode.el
+++ dotfiles/emacs.d/jsoa/python-mode.el
@@ -24,7 +24,10 @@
 ;;    (insert-pair)))
 
 ;;; bind RET to py-newline-and-indent
+;;; bind f7 to pylint and f8 to pep8
 (add-hook 'python-mode-hook '(lambda ()
+     (define-key python-mode-map [(f7)] 'python-pylint)
+     (define-key python-mode-map [(f8)] 'python-pep8)
      (define-key python-mode-map "\C-m" 'newline-and-indent)))
 
 ;; (add-hook 'python-mode-hook
