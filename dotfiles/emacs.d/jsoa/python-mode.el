11d490ad84359a23768a798cb5d0b67dc6a9fff4
Jose Soares <josefsoares@gmail.com>
2013-03-15 10:39:03 -0400

    Added flycheck-mode and enabled it on python-mode

--- dotfiles/emacs.d/jsoa/python-mode.el
+++ dotfiles/emacs.d/jsoa/python-mode.el
@@ -25,7 +25,9 @@
 
 ;;; bind RET to py-newline-and-indent
 ;;; bind f7 to pylint and f8 to pep8
+;;; enabled flycheck mode for python
 (add-hook 'python-mode-hook '(lambda ()
+     (flycheck-mode)
      (define-key python-mode-map [(f7)] 'python-pylint)
      (define-key python-mode-map [(f8)] 'python-pep8)
      (define-key python-mode-map "\C-m" 'newline-and-indent)))
