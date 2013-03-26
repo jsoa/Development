58a599a109523e867071307eef997d5fc9f2403c
Jose Soares <josefsoares@gmail.com>
2013-03-26 13:48:56 -0400

    [-] Removed nxhtml; enabled web-mode

--- dotfiles/emacs.d/jsoa/nxhtml.el
+++ dotfiles/emacs.d/jsoa/nxhtml.el
@@ -1,10 +1,10 @@
-(if (boundp 'image-types)
-    nil
-    (defvar image-types nil))
+;; (if (boundp 'image-types)
+;;     nil
+;;     (defvar image-types nil))
 
-(load "../vendor/nxhtml/autostart")
+;; (load "../vendor/nxhtml/autostart")
 
-(setq auto-mode-alist
-      (append '(("\\.html?$" . django-html-mumamo-mode)) auto-mode-alist))
-(setq mumamo-background-colors nil)
-(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mumamo-mode))
+;; (setq auto-mode-alist
+;;       (append '(("\\.html?$" . django-html-mumamo-mode)) auto-mode-alist))
+;; (setq mumamo-background-colors nil)
+;; (add-to-list 'auto-mode-alist '("\\.html$" . django-html-mumamo-mode))
