5e992cb3515cc24de60917544c1e39f0cf0c245b
Jose Soares <josefsoares@gmail.com>
2013-04-11 15:24:34 -0400

    [-] Tweaks to web-mode

--- dotfiles/emacs.d/jsoa/web-mode.el
+++ dotfiles/emacs.d/jsoa/web-mode.el
@@ -1,3 +1,14 @@
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+
+
+(add-hook 'web-mode-hook '(lambda ()
+  (local-set-key (kbd "RET") 'newline-and-indent)
+
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-indent-style 2)
+
+))
