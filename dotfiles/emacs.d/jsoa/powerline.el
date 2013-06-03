4c0a714327e7313476065f06ea8981cbdc7e2cba
Jose Soa <josefsoares@gmail.com>
2013-06-03 15:35:13 -0400

    [-] Updated powerline colors again

--- dotfiles/emacs.d/jsoa/powerline.el
+++ dotfiles/emacs.d/jsoa/powerline.el
@@ -1,9 +1,15 @@
 (powerline-default-theme)
 
 (custom-set-faces
- '(mode-line ((t (:background "#5c5cff" :foreground "#000000" :box nil))))
- '(powerline-active1 ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
- '(powerline-active2 ((t (:foreground "#f9f9f9" :background "#666666" :box nil))))
+  '(mode-line ((t (:background "#5c5cff" :foreground "#000000" :box nil))))
+
+  ;; Keep the colors for inactive and active the same, just change the primary
+  ;; mode-line color (above)
+  '(powerline-active1 ((t (:foreground "grey80" :background "gray10" :box nil))))
+  '(powerline-active2 ((t (:foreground "grey80" :background "grey20" :box nil))))
+  '(powerline-inactive1 ((t (:foreground "grey80" :background "grey10" :box nil))))
+  '(powerline-inactive2 ((t (:foreground "grey80" :background "grey20" :box nil))))
+
 )
 
 (powerline-center-theme)
