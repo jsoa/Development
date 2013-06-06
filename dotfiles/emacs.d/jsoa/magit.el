e9615a1fbfab6ed3068a6b204d96a494d1b7bfc3
Jose Soa <josefsoares@gmail.com>
2013-06-06 16:00:23 -0400

    [-] Added some custom colors to magit status buffer

--- dotfiles/emacs.d/jsoa/magit.el
+++ dotfiles/emacs.d/jsoa/magit.el
@@ -1,3 +1,6 @@
 (require 'magit-blame)
 
-
+;; Some custom magit status buffer colors
+(custom-set-faces
+ '(magit-branch ((t (:foreground "brightblue" :weight bold))))
+ '(magit-section-title ((t (:foreground "yellow")))))
