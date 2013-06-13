d4e7949eb50b8e813b928c0fb5b584415da006a7
Jose Soa <josefsoares@gmail.com>
2013-06-12 23:34:49 -0400

    [-] Coverted the colors of git-gutter

--- dotfiles/emacs.d/jsoa/git-gutter.el
+++ dotfiles/emacs.d/jsoa/git-gutter.el
@@ -1,22 +1,23 @@
 (global-git-gutter-mode t)
 
-;; (setq git-gutter:window-width 2)
+(setq git-gutter:window-width 1)
 
 (setq git-gutter:modified-sign "|")
 (setq git-gutter:added-sign "|")
 (setq git-gutter:deleted-sign "|")
 
-(set-face-background 'git-gutter:modified "color-234")
-(set-face-background 'git-gutter:added "color-234")
-(set-face-background 'git-gutter:deleted "color-234")
+(set-face-background 'git-gutter:modified "#131313")
+(set-face-background 'git-gutter:added "#131313")
+(set-face-background 'git-gutter:deleted "#131313")
+
 
 (set-face-foreground 'git-gutter:modified "blue")
 (set-face-foreground 'git-gutter:added "green")
 (set-face-foreground 'git-gutter:deleted "red")
 
 (setq git-gutter:unchanged-sign "|")
-(set-face-background 'git-gutter:unchanged "color-234")
-(set-face-foreground 'git-gutter:unchanged "color-234")
+(set-face-background 'git-gutter:unchanged "#131313")
+(set-face-foreground 'git-gutter:unchanged "#131313")
 
 ;; (setq git-gutter:separator-sign " ")
 ;; (set-face-background 'git-gutter:separator "color-234")
