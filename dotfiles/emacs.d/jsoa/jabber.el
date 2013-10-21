bea47a64e13d98878142be4d1c12aa39c88cd1a8
Jose Soa <josefsoares@gmail.com>
2013-10-21 10:08:42 -0400

    [-] Added jabber notifications via notify-send

--- dotfiles/emacs.d/jsoa/jabber.el
+++ dotfiles/emacs.d/jsoa/jabber.el
@@ -23,6 +23,26 @@
 (setq jabber-chat-buffer-show-avatar nil)
 
 
+(defvar libnotify-program "/usr/bin/notify-send")
+
+(defun notify-send (title message)
+  (start-process "notify" " notify"
+		 libnotify-program "--expire-time=4000" title message))
+
+(defun libnotify-jabber-notify (from buf text proposed-alert)
+  "(jabber.el hook) Notify of new Jabber chat messages via libnotify"
+  (when (or jabber-message-alert-same-buffer
+            (not (memq (selected-window) (get-buffer-window-list buf))))
+    (if (jabber-muc-sender-p from)
+        (notify-send (format "(PM) %s"
+                       (jabber-jid-displayname (jabber-jid-user from)))
+               (format "%s: %s" (jabber-jid-resource from) text)))
+      (notify-send (format "%s" (jabber-jid-displayname from))
+             text))
+)
+
+(add-hook 'jabber-alert-message-hooks 'libnotify-jabber-notify)
+
 ;; Mode hook functions
 (defun jsoa/jabber-connect-hook (jc)
   (jabber-send-presence "" "Online" 10)
