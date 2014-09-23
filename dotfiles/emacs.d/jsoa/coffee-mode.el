6a94d06f51e892ee2ec78c0670bf1598f39105fd
Jose Soares <jose@linux.com>
2014-09-23 11:31:28 -0400

    [-] Tweaked coffee-mode

--- dotfiles/emacs.d/jsoa/coffee-mode.el
+++ dotfiles/emacs.d/jsoa/coffee-mode.el
@@ -1,10 +1,16 @@
 (defun coffee-custom ()
   "coffee-mode-hook"
  ;; (coffee-cos-mode t)
- (set (make-local-variable 'tab-width) 2)
+ (set coffee-cos-mode 'nil)
+
+ (make-local-variable 'tab-width)
+ (set 'tab-width 2)
  (setq coffee-args-compile '("-c" "-o" "../")))
 
-(add-hook 'coffee-mode-hook
-  '(lambda() (coffee-custom)))
+;; (add-hook 'coffee-mode-hook
+;;   '(lambda() (coffee-custom)))
 
-;; (add-hook 'coffee-mode-hook '(lambda () (coffee-cos-mode t)))
+(add-hook 'coffee-mode-hook '(lambda ()
+  (setq coffee-cos-mode nil)
+  (setq coffee-tab-width 2)
+))
